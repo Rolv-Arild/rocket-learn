bd07d557b02b71329405a8e1977079131b9fa44d
Soren <danieljosephdowns@gmail.com>
2021-08-12 18:51:42 -0700

    changed tester to work with lunar lander

--- rocket_learn/example_main.py
+++ rocket_learn/example_main.py
@@ -1,9 +1,7 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
-from rlgym.utils.obs_builders import AdvancedObs
-from rlgym.utils.reward_functions.common_rewards import TouchBallReward, VelocityReward
-from rlgym.utils.terminal_conditions.common_conditions import TimeoutCondition
+
 from rocket_learn.learner import PPO
 from rocket_learn.rollout_generator.simple_rollout_generator import SimpleRolloutGenerator
 
@@ -14,7 +12,7 @@ class SplitLayer(nn.Module):
         if splits is not None:
             self.splits = splits
         else:
-            self.splits = (3, 3, 3, 3, 3, 2, 2, 2)
+            self.splits = (3, 3, 3, 3)
 
     def forward(self, x):
         return torch.split(x, self.splits, dim=-1)
@@ -36,29 +34,29 @@ self_play = True
 
 state_dim = 8
 print(state_dim)
-action_dim = 8
+action_dim = 4
 
 critic = nn.Sequential(
-    nn.Linear(state_dim, 64),
-    nn.Tanh(),
-    nn.Linear(64, 64),
-    nn.Tanh(),
-    nn.Linear(64, 1)
+    nn.Linear(state_dim, 256),
+    nn.ReLU(),
+    nn.Linear(256, 256),
+    nn.ReLU(),
+    nn.Linear(256, 1)
 )
 
 actor = nn.Sequential(
-    nn.Linear(state_dim, 64),
-    nn.Tanh(),
-    nn.Linear(64, 64),
-    nn.Tanh(),
-    nn.Linear(64, action_dim),
+    nn.Linear(state_dim, 256),
+    nn.ReLU(),
+    nn.Linear(256, 256),
+    nn.ReLU(),
+    nn.Linear(256, action_dim),
     SplitLayer((4,))
 )
 
 if __name__ == '__main__':
     rollout_gen = SimpleRolloutGenerator(None, team_size=1, self_play=True)
 
-    alg = PPO(rollout_gen, actor, critic, n_rollouts=32)
+    alg = PPO(rollout_gen, actor, critic, batch_size=32, n_steps=4096, lr_critic=.001, lr_actor=.001, epochs=1)
     rollout_gen.agent = alg.agent
     # rl_path = "C:\\EpicGames\\rocketleague\\Binaries\\Win64\\RocketLeague.exe"
     log_dir = "E:\\log_directory\\"
