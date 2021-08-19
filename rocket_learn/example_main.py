497cd7f539da7ca0fb6054c5c810002eda189dfa
Matthew Allen <AechPro@users.noreply.github.com>
2021-08-19 14:39:27 -0400

    Hyperparameter changes

--- rocket_learn/example_main.py
+++ rocket_learn/example_main.py
@@ -39,19 +39,19 @@ print(state_dim)
 action_dim = 8
 
 critic = nn.Sequential(
-    nn.Linear(state_dim, 64),
-    nn.Tanh(),
-    nn.Linear(64, 64),
-    nn.Tanh(),
-    nn.Linear(64, 1)
+    nn.Linear(state_dim, 256),
+    nn.ReLU(),
+    nn.Linear(256, 256),
+    nn.ReLU(),
+    nn.Linear(256, 1)
 )
 
 actor = nn.Sequential(
-    nn.Linear(state_dim, 128),
-    nn.Tanh(),
-    nn.Linear(128, 128),
-    nn.Tanh(),
-    nn.Linear(128, 21),
+    nn.Linear(state_dim, 64),
+    nn.ReLU(),
+    nn.Linear(64, 64),
+    nn.ReLU(),
+    nn.Linear(64, 21),
     SplitLayer()
 )
 
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     # rollout_gen = SimpleRolloutGenerator(None, **get_match_args())
     rollout_gen = RedisRolloutGenerator()
 
-    alg = PPO(rollout_gen, actor, critic)
+    alg = PPO(rollout_gen, actor, critic, n_steps=5000, batch_size=500, lr_critic=3e-4, lr_actor=3e-4, epochs=10)
     # rollout_gen.agent = alg.agent
     # rl_path = "C:\\EpicGames\\rocketleague\\Binaries\\Win64\\RocketLeague.exe"
     log_dir = "E:\\log_directory\\"
