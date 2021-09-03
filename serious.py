8d64633c2c6143e14dff64f6f6a28cbce48f6db5
Rolv-Arild <rolv_arild@hotmail.com>
2021-09-03 13:07:30 +0200

    Started on reward

--- serious.py
+++ serious.py
@@ -7,14 +7,19 @@ from typing import Any
 import numpy as np
 import torch.jit
 from earl_pytorch import EARLPerceiver, ControlsPredictorDiscrete
+from rlgym_tools.extra_rewards.distribute_rewards import DistributeRewards
 from torch.nn import Sequential, Linear
 
 import wandb
 from rlgym.envs import Match
 from rlgym.utils import ObsBuilder, TerminalCondition, RewardFunction, StateSetter
-from rlgym.utils.common_values import ORANGE_TEAM, BOOST_LOCATIONS, BLUE_TEAM
+from rlgym.utils.common_values import ORANGE_TEAM, BOOST_LOCATIONS, BLUE_TEAM, BLUE_GOAL_BACK, ORANGE_GOAL_BACK, \
+    BALL_MAX_SPEED
 from rlgym.utils.gamestates import PlayerData, GameState
+from rlgym.utils.math import cosine_similarity, scalar_projection
+from rlgym.utils.reward_functions.common_rewards import EventReward
 from rlgym.utils.state_setters import StateWrapper
+from rlgym.utils.terminal_conditions.common_conditions import NoTouchTimeoutCondition, GoalScoredCondition
 from rocket_learn.ppo import PPOAgent, PPO
 from rocket_learn.rollout_generator.redis_rollout_generator import RedisRolloutGenerator
 
@@ -41,9 +46,9 @@ class SeriousObsBuilder(ObsBuilder):
     def update_if_new_state(self, state: GameState):
         if self.current_state == state:  # No need to update
             return
-        
+
         qkv = np.zeros((1 + self.n_players + len(state.boost_pads), 24))  # Ball, players, boosts
-        
+
         # Add ball
         n = 0
         ball = state.ball
@@ -67,24 +72,24 @@ class SeriousObsBuilder(ObsBuilder):
             qkv[n, 14:17] = car_data.up()
             qkv[n, 17:20] = car_data.angular_velocity
             qkv[n, 20] = player.boost_amount
-#             qkv[n, 21] = player.is_demoed
+            #             qkv[n, 21] = player.is_demoed
             demos[n - 1] = player.is_demoed  # Keep track for demo timer
             qkv[n, 22] = player.on_ground
             qkv[n, 23] = player.has_flip
             n += 1
-        
+
         # Add boost pads
         n = 1 + self.n_players
         boost_pads = state.boost_pads
         qkv[n:, 4] = 1  # is_boost
         qkv[n:, 5:8] = self._boost_locations
         qkv[n:, 20] = 0.12 + 0.88 * (self._boost_locations[:, 2] > 72)  # Boost amount
-#         qkv[n:, 21] = boost_pads
-        
+        #         qkv[n:, 21] = boost_pads
+
         # Boost and demo timers
-        new_boost_grabs = state.boost_pads & (self.boost_timers == 0)  # New boost grabs since last frame
+        new_boost_grabs = boost_pads & (self.boost_timers == 0)  # New boost grabs since last frame
         self.boost_timers[new_boost_grabs] = 0.4 + 0.6 * (self._boost_locations[new_boost_grabs, 2] > 72)
-        self.boost_timers *= state.boost_pads  # Make sure we have zeros right
+        self.boost_timers *= boost_pads  # Make sure we have zeros right
         qkv[1 + self.n_players:, 21] = self.boost_timers
         self.boost_timers -= self.tick_skip / 1200  # Pre-normalized, 120 fps for 10 seconds
         self.boost_timers[self.boost_timers < 0] = 0
@@ -92,16 +97,15 @@ class SeriousObsBuilder(ObsBuilder):
         new_demos = demos & (self.demo_timers == 0)
         self.demo_timers[new_demos] = 0.3
         self.demo_timers *= demos
-        qkv[1 : 1 + self.n_players, 21] = self.demo_timers
+        qkv[1: 1 + self.n_players, 21] = self.demo_timers
         self.demo_timers -= self.tick_skip / 1200
         self.demo_timers[self.demo_timers < 0] = 0
-        
+
         # Store results
         self.current_qkv = qkv / self._norm
         mask = np.zeros(qkv.shape[0])
         mask[1 + len(state.players):1 + self.n_players] = 1
         self.current_mask = mask
-
 
     def build_obs(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> Any:
         self.update_if_new_state(state)
@@ -113,7 +117,7 @@ class SeriousObsBuilder(ObsBuilder):
         main_n = state.players.index(player)
         qkv[main_n, 0] = 1  # is_main
         if invert:
-            qkv[:, [1, 2]] = qkv[:, [2, 1]]  # Swap blue/orange
+            qkv[:, (1, 2)] = qkv[:, (2, 1)]  # Swap blue/orange
             qkv *= self._invert  # Negate x and y values
 
         q = qkv[main_n, :]
@@ -126,24 +130,42 @@ class SeriousObsBuilder(ObsBuilder):
         return q, kv, mask
 
 
-class SeriousTerminalCondition(TerminalCondition):  # What a name
-    # Probably just use simple goal and no touch terminals
-    def reset(self, initial_state: GameState):
-        pass
-
-    def is_terminal(self, current_state: GameState) -> bool:
-        pass
+def SeriousTerminalCondition(tick_skip=8):
+    return [NoTouchTimeoutCondition(round(30 * tick_skip / 120)), GoalScoredCondition()]
 
 
 class SeriousRewardFunction(RewardFunction):
+    def __init__(self):
+        self.last_state = None
+        self.current_state = None
+
     # Something like DistributeRewards(EventReward(goal=4, shot=4, save=4, demo=4, touch=1))
     # but find a way to reduce dribble abuse
     # Also add std/max/min rewards to log so we can actually see progress
     def reset(self, initial_state: GameState):
-        pass
+        self.last_state = None
+        self.current_state = initial_state
 
     def get_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> float:
-        pass
+        if state != self.current_state:
+            self.last_state = self.current_state
+            self.current_state = state
+        if self.last_state is None:
+            return 0
+
+        rew = 0
+        for p in self.last_state.players:
+            if p.car_id == player.car_id:
+                rew += 10 * (player.match_goals - p.match_shots) + \
+                       5 * (player.match_shots - p.match_shots) + \
+                       5 * (player.match_saves - p.match_saves) + \
+                       5 * (player.match_demolishes - p.match_demolishes)
+                break
+        if player.ball_touched:
+            target = np.array(BLUE_GOAL_BACK if player.team_num == BLUE_TEAM else ORANGE_GOAL_BACK)
+            curr_vel = self.current_state.ball.linear_velocity
+            last_vel = self.last_state.ball.linear_velocity
+            rew += scalar_projection(curr_vel - last_vel, target - state.ball.position) / BALL_MAX_SPEED
 
 
 class SeriousStateSetter(StateSetter):
