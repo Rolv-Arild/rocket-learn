ff67e2d47207a889d574d4f518e7711efa79d2fb
shashankm <shashankmurthyjobs@gmail.com>
2021-09-12 20:16:56 -0500

    general fixes and moved util functions to util file

--- rocket_learn/utils/util.py
+++ rocket_learn/utils/util.py
@@ -2,7 +2,6 @@ from typing import List, Tuple
 
 import numpy as np
 import torch.distributions
-from torch.distributions import Distribution, Categorical
 
 from rlgym.gym import Gym
 from rocket_learn.utils.experiencebuffer import ExperienceBuffer
