Move advantage calculation Add gradient averaging Add TrueSkill plot with confidence Fix big error in serious obs Made limit_threads default to True

rocket_learn/ppo.py
@@ -198,12 +198,11 @@ class PPO:
                     v_targets[step] = v_target
 
             returns = advantages + values
-            advantages = (advantages - th.mean(advantages)) / (th.std(advantages) + 1e-8)
 
             obs_tensors.append(obs_tensor)
             act_tensors.append(act_tensor)
             log_prob_tensors.append(log_prob_tensor)
-            advantage_tensors.append(advantages)
+            # advantage_tensors.append(advantages)
             returns_tensors.append(returns)
             v_target_tensors.append(v_targets)
             rewards_tensors.append(rew_tensor)
@@ -227,7 +226,7 @@ class PPO:
             obs_tensor = th.cat(obs_tensors).float()
         act_tensor = th.cat(act_tensors)
         log_prob_tensor = th.cat(log_prob_tensors).float()
-        advantages_tensor = th.cat(advantage_tensors)
+        # advantages_tensor = th.cat(advantage_tensors)
         returns_tensor = th.cat(returns_tensors)
 
         # rewards_tensor = th.cat(rewards_tensors)
@@ -250,23 +249,20 @@ class PPO:
         tot_value_loss = 0
         n = 0
 
-        pr = cProfile.Profile()
-        pr.enable()
-
         pb = tqdm.tqdm(desc="Training network", total=self.epochs * self.batch_size, position=0, leave=True)
 
         self.agent.optimizer.zero_grad()
         for e in range(self.epochs):
             # this is mostly pulled from sb3
 
-            indices = torch.randperm(advantages_tensor.shape[0])[:self.batch_size]
+            indices = torch.randperm(returns_tensor.shape[0])[:self.batch_size]
             if isinstance(obs_tensor, tuple):
                 obs_batch = tuple(o[indices] for o in obs_tensor)
             else:
                 obs_batch = obs_tensor[indices]
             act_batch = act_tensor[indices]
             log_prob_batch = log_prob_tensor[indices]
-            advantages_batch = advantages_tensor[indices]
+            # advantages_batch = advantages_tensor[indices]
             returns_batch = returns_tensor[indices]
 
             for i in range(0, self.batch_size, self.minibatch_size):
@@ -278,7 +274,7 @@ class PPO:
                     obs = obs_batch[i: i + self.minibatch_size].to(self.device)
 
                 act = act_batch[i: i + self.minibatch_size].to(self.device)
-                adv = advantages_batch[i:i + self.minibatch_size].to(self.device)
+                # adv = advantages_batch[i:i + self.minibatch_size].to(self.device)
                 ret = returns_batch[i: i + self.minibatch_size].to(self.device)
 
                 old_log_prob = log_prob_batch[i: i + self.minibatch_size].to(self.device)
@@ -287,14 +283,17 @@ class PPO:
                 log_prob, entropy = self.evaluate_actions(obs, act)  # Assuming obs and actions as input
                 ratio = torch.exp(log_prob - old_log_prob)
 
+                values_pred = self.agent.critic(obs)
+                values_pred = th.squeeze(values_pred)
+                adv = ret - values_pred
+                adv = (adv - th.mean(adv)) / (th.std(adv) + 1e-8)
+
                 # clipped surrogate loss
                 policy_loss_1 = adv * ratio
                 policy_loss_2 = adv * th.clamp(ratio, 1 - self.clip_range, 1 + self.clip_range)
                 policy_loss = -torch.min(policy_loss_1, policy_loss_2).mean()
 
                 # **If we want value clipping, add it here**
-                values_pred = self.agent.critic(obs)
-                values_pred = th.squeeze(values_pred)
                 value_loss = F.mse_loss(ret, values_pred)
 
                 if entropy is None:
@@ -303,9 +302,8 @@ class PPO:
                 else:
                     entropy_loss = entropy
 
-                loss = policy_loss + self.ent_coef * entropy_loss + self.vf_coef * value_loss
-                if not torch.isfinite(loss).all():
-                    print("And I oop")
+                loss = ((policy_loss + self.ent_coef * entropy_loss + self.vf_coef * value_loss)
+                        / (self.batch_size / self.minibatch_size))
 
                 loss.backward()
 
@@ -323,11 +321,6 @@ class PPO:
 
             self.agent.optimizer.step()
             self.agent.optimizer.zero_grad()
-
-        pr.disable()
-        sortby = SortKey.CUMULATIVE
-        ps = pstats.Stats(pr).sort_stats(sortby).dump_stats("policy_update.pstats")
-        print("Hei")
 
         self.logger.log({
             "loss": tot_loss / n,

rocket_learn/rollout_generator/redis_rollout_generator.py
@@ -7,7 +7,10 @@ from uuid import uuid4
 import msgpack
 import msgpack_numpy as m
 import numpy as np
+import matplotlib.pyplot  # noqa
 import wandb
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 from redis import Redis
 from redis.exceptions import ResponseError
 from trueskill import Rating, rate
@@ -122,7 +125,7 @@ class RedisRolloutGenerator(BaseRolloutGenerator):
                                         (sum(r.sigma for r in ratings) / len(ratings)))
                     if version > 0:  # Old
                         self.redis.lset(QUALITIES, version, _serialize(tuple(avg_rating)))
-                    elif version == latest_version:
+                    elif abs(version - latest_version) <= 1:
                         self.redis.set(QUALITY_LATEST, _serialize(tuple(avg_rating)))
 
                 yield from rollouts
@@ -134,11 +137,21 @@ class RedisRolloutGenerator(BaseRolloutGenerator):
         ratings = [Rating(*_unserialize(v)) for v in self.redis.lrange(QUALITIES, 0, -1)]
         if ratings:
             mus = np.array([r.mu for r in ratings])
+            conf = np.array([r.sigma for r in ratings])
+            fig = Figure()
+            ax: Axes = fig.subplots()
+            ax.plot(np.arange(len(ratings)), mus, color="royalblue")
+            ax.fill_between(np.arange(len(ratings)), mus + 3 * conf, mus - 3 * conf, color="lightsteelblue")
+            ax.set_xlabel("Iteration")
+            ax.set_ylabel("TrueSkill")
+            ax.set_title("Qualities")
+
             self.logger.log({
-                "qualities": wandb.plot.line_series(np.arange(len(mus)), [mus],
-                                                    # noqa
-                                                    ["quality"], "Qualities", "version")
-            })
+                "qualities": wandb.Image(fig),
+                # "qualities": wandb.plot.line_series(np.arange(len(mus)), [mus],
+                #                                     # noqa
+                #                                     ["quality"], "Qualities", "version")
+            }, commit=False)
             quality = Rating(ratings[-1].mu)  # Same mu, reset sigma
         else:
             quality = Rating(0, 1)  # First agent is fixed at 0
@@ -242,6 +255,7 @@ class RedisRolloutWorker:
                     agents.append((selected_agent, version, prob))
 
             np.random.shuffle(agents)
+            print("Generating rollout with versions:", [v for a, v, p in agents])
 
             rollouts, result = util.generate_episode(self.env, [agent for agent, version, prob in agents])
 

serious.py
@@ -14,8 +14,7 @@ from torch.nn import Linear
 import wandb
 from rlgym.envs import Match
 from rlgym.utils import ObsBuilder, RewardFunction
-from rlgym.utils.common_values import ORANGE_TEAM, BOOST_LOCATIONS, BLUE_TEAM, BLUE_GOAL_BACK, ORANGE_GOAL_BACK, \
-    BALL_MAX_SPEED, CEILING_Z
+from rlgym.utils.common_values import ORANGE_TEAM, BOOST_LOCATIONS, BLUE_TEAM, BALL_MAX_SPEED, CEILING_Z
 from rlgym.utils.gamestates import PlayerData, GameState
 from rlgym.utils.state_setters import DefaultState
 from rlgym.utils.terminal_conditions.common_conditions import NoTouchTimeoutCondition, GoalScoredCondition
@@ -125,7 +124,7 @@ class SeriousObsBuilder(ObsBuilder):
         qkv = self.current_qkv.copy()
         mask = self.current_mask.copy()
 
-        main_n = state.players.index(player)
+        main_n = state.players.index(player) + 1
         qkv[0, main_n, 0] = 1  # is_main
         if invert:
             qkv[0, :, (1, 2)] = qkv[0, :, (2, 1)]  # Swap blue/orange
@@ -187,7 +186,7 @@ class SeriousRewardFunction(RewardFunction):
             # Discounted future reward in limit would be (0.5 / (1 * 15)) / (1 - 0.995) = 6.67 as a generous estimate
             # Pros are generally around maybe 400 bcpm, which would be 0.44 limit
             if new_p.ball_touched:
-                target = np.array(ORANGE_GOAL_BACK if new_p.team_num == BLUE_TEAM else BLUE_GOAL_BACK)
+                # target = np.array(ORANGE_GOAL_BACK if new_p.team_num == BLUE_TEAM else BLUE_GOAL_BACK)
                 curr_vel = self.current_state.ball.linear_velocity
                 last_vel = self.last_state.ball.linear_velocity
                 # On ground it gets about 0.05 just for touching, as well as some extra for the speed it produces
@@ -209,8 +208,6 @@ class SeriousRewardFunction(RewardFunction):
         self.rewards = np.zeros_like(rewards)
         self.rewards[blue_mask] = (1 - self.team_spirit) * blue_rewards + self.team_spirit * blue_mean - orange_mean
         self.rewards[orange_mask] = (1 - self.team_spirit) * orange_rewards + self.team_spirit * orange_mean - blue_mean
-        if (self.rewards > self.goal_w + 1).any():
-            print("Hei")
 
     def get_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> float:
         self._maybe_update_rewards(state)
@@ -253,7 +250,7 @@ class Necto(nn.Module):  # Wraps earl + an output and takes only a single input
         return torch.squeeze(res)
 
 
-def make_worker(host, name, limit_threads=False):
+def make_worker(host, name, limit_threads=True):
     if limit_threads:
         torch.set_num_threads(1)
     r = Redis(host=host, password="rocket-learn")
@@ -272,7 +269,7 @@ if __name__ == "__main__":
 
     redis = Redis(password="rocket-learn")
     redis.delete(WORKER_COUNTER)  # Reset to 0
-    rollout_gen = RedisRolloutGenerator(redis, save_every=10, logger=logger)
+    rollout_gen = RedisRolloutGenerator(redis, save_every=1, logger=logger)
 
     # jit models can't be pickled
     # ex_inp = (
@@ -285,15 +282,15 @@ if __name__ == "__main__":
     #     func=Necto(EARLPerceiver(256, query_features=32, key_value_features=24), ControlsPredictorDiscrete(256)),
     #     example_inputs=ex_inp
     # )
-    critic = Necto(EARLPerceiver(128, query_features=32, key_value_features=24),
+    critic = Necto(EARLPerceiver(128, 1, 4, 1, query_features=32, key_value_features=24),
                    Linear(128, 1))
-    actor = DiscretePolicy(Necto(EARLPerceiver(256, query_features=32, key_value_features=24),
+    actor = DiscretePolicy(Necto(EARLPerceiver(256, 1, 4, 1, query_features=32, key_value_features=24),
                                  ControlsPredictorDiscrete(256)))
 
     lr = 1e-5
     optim = torch.optim.Adam([
-        {"params": actor.parameters(), "lr": 1e-5},
-        {"params": critic.parameters(), "lr": 1e-5}
+        {"params": actor.parameters(), "lr": lr},
+        {"params": critic.parameters(), "lr": lr}
     ])
 
     agent = ActorCriticAgent(actor=actor, critic=critic, optimizer=optim)
@@ -301,15 +298,15 @@ if __name__ == "__main__":
     alg = PPO(
         rollout_gen,
         agent,
-        n_steps=100_000,
-        batch_size=20_000,
+        n_steps=1_000_000,
+        batch_size=200_000,
         minibatch_size=10_000,
-        gamma=0.995,
         epochs=10,
-        logger=logger
+        gamma=0.995,
+        logger=logger,
     )
 
     log_dir = "E:\\log_directory\\"
     repo_dir = "E:\\repo_directory\\"
 
-    alg.run()
+    alg.run(epochs_per_save=1)