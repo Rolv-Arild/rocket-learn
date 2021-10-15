8ce7c3876d603c25d8984488a720a2627ac132e9
unknown <danieljosephdowns@gmail.com>
2021-10-14 20:36:56 -0500

    increment version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ Learning!
 
 setup(
    name='rocket_learn',
-   version='0.1',
+   version='0.1.2',
    description='Rocket Learn',
    author=['Rolv Arild', 'Daniel Downs'],
    url='https://github.com/Rolv-Arild/rocket-learn',
