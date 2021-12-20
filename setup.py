d7a6687ae3d24b678aa20fb7bb0fb0ea9747546b
Rolv-Arild <rolv_arild@hotmail.com>
2021-12-20 18:03:06 +0100

    Reduce number of processes for receiving gamestates Add check to limit rollouts piling up in case learner can't keep up or has crashed

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ Learning!
 
 setup(
    name='rocket_learn',
-   version='0.1.7',
+   version='0.1.8',
    description='Rocket Learn',
    author=['Rolv Arild', 'Daniel Downs'],
    url='https://github.com/Rolv-Arild/rocket-learn',
