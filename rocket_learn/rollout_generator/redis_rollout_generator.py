790967d3d5230c51b70e6d8bed58682c59b033b3
Rolv-Arild <rolv_arild@hotmail.com>
2021-12-26 19:37:17 +0100

    Fix

--- rocket_learn/rollout_generator/redis_rollout_generator.py
+++ rocket_learn/rollout_generator/redis_rollout_generator.py
@@ -438,7 +438,7 @@ class RedisRolloutWorker:
 
             state = rollouts[0].infos[-1]["state"]
             goal_speed = np.linalg.norm(state.ball.linear_velocity) * 0.036  # kph
-            str_result = ('+' + result if result > 0 else str(result))
+            str_result = ('+' + result if result > 0 else "") + str(result)
             post_stats = f"Rollout finished after {len(rollouts[0])} steps, result was {str_result}"
             if result != 0:
                 post_stats += f", goal speed: {goal_speed} kph"
