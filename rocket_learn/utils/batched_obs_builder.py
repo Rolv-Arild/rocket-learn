5393ac33c87cfd510f5e2f7099ee65f185d69c19
Rolv-Arild <rolv_arild@hotmail.com>
2022-01-15 04:10:30 +0100

    Update batched obs builder

--- rocket_learn/utils/batched_obs_builder.py
+++ rocket_learn/utils/batched_obs_builder.py
@@ -16,7 +16,7 @@ class BatchedObsBuilder(ObsBuilder):
     def batched_build_obs(self, encoded_states: np.ndarray):
         raise NotImplementedError
 
-    def add_action(self, previous_actions: np.ndarray, player_index=None):
+    def add_actions(self, obs: Any, previous_actions: np.ndarray, player_index=None):
         # Modify current obs to include action
         # player_index=None means actions for all players should be provided
         raise NotImplementedError
@@ -25,17 +25,21 @@ class BatchedObsBuilder(ObsBuilder):
         raise NotImplementedError
 
     def reset(self, initial_state: GameState):
-        self.current_state = None
+        self.current_state = False
         self.current_obs = None
         self._reset(initial_state)
 
     def build_obs(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> Any:
-        if state == self.current_state:
-            for i, p in enumerate(state.players):
-                self.add_action(previous_action, i)
-                if p == player:
-                    return self.current_obs[i]
-        else:
+        if self.current_state is None:
+            self.current_state = state
+            return np.zeros(0)  # Make Aech happy
+        if state != self.current_state:
             self.current_obs = self.batched_build_obs(
                 np.expand_dims(encode_gamestate(state), axis=0)
             )
+            self.current_state = state
+
+        for i, p in enumerate(state.players):
+            if p == player:
+                self.add_actions(self.current_obs, previous_action, i)
+                return self.current_obs[i]
