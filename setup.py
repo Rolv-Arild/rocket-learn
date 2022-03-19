0c738a76a407aa03cea6bbb738b257d5d90fbba0
unknown <danieljosephdowns@gmail.com>
2022-03-19 08:49:08 -0500

    updated version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ Learning!
 
 setup(
    name='rocket_learn',
-   version='0.1.30',
+   version='0.1.31',
    description='Rocket Learn',
    author=['Rolv Arild', 'Daniel Downs'],
    url='https://github.com/Rolv-Arild/rocket-learn',
