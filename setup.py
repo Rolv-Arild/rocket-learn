fd266e8b58ccf9b393c545a473daabf8a25cd8f8
Rolv-Arild <rolv_arild@hotmail.com>
2022-04-11 22:48:36 +0200

    Update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ Learning!
 
 setup(
    name='rocket_learn',
-   version='0.1.35',
+   version='0.1.36',
    description='Rocket Learn',
    author=['Rolv Arild', 'Daniel Downs'],
    url='https://github.com/Rolv-Arild/rocket-learn',
