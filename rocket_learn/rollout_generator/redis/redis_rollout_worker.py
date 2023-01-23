6b9b8dd41e593eefc911fe1518626df0a83439be
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2023-01-23 08:56:39 -0500

    typo

--- rocket_learn/rollout_generator/redis/redis_rollout_worker.py
+++ rocket_learn/rollout_generator/redis/redis_rollout_worker.py
@@ -323,7 +323,7 @@ class RedisRolloutWorker:
                 episode_exp = len(rollouts[0].observations) * len(rollouts)
                 self.total_steps_generated += episode_exp
                 old_exp = self.mean_exp_grant[f"{blue}v{orange}"]
-                self.mean_exp_grant = ((episode_exp - old_exp) * self.ema_alpha) + old_exp
+                self.mean_exp_grant[f"{blue}v{orange}"] = ((episode_exp - old_exp) * self.ema_alpha) + old_exp
                 post_stats = f"Rollout finished after {len(rollouts[0].observations)} steps ({self.total_steps_generated} total steps), result was {str_result}"
                 if result != 0:
                     post_stats += f", goal speed: {goal_speed:.2f} kph"
