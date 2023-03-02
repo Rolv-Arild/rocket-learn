b5e158351ce424426ca0e48027bef7d281d15b0e
Rolv <rolv_arild@hotmail.com>
2023-03-02 11:47:59 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ Learning!
 
 setup(
    name='rocket_learn',
-   version='0.2.4',
+   version='0.2.5',
    description='Rocket Learn',
    author=['Rolv Arild', 'Daniel Downs'],
    url='https://github.com/Rolv-Arild/rocket-learn',
