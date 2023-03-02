Add optional decay for KL against fixed models Log raw KL and coef instead of combination Add EMA and adjustments to experience counts Add option for full-team evaluation matches Add stat trackers for saves and shots Update GoalSpeed tracker

rocket_learn/ppo.py
@@ -4,7 +4,7 @@ import os
 import pstats
 import time
 import sys
-from typing import Iterator, List, Tuple
+from typing import Iterator, List, Tuple, Union
 
 import numba
 import numpy as np
@@ -62,7 +62,7 @@ class PPO:
             logger=None,
             device="cuda",
             zero_grads_with_none=False,
-            kl_models_weights: List[Tuple[Policy, float]] = None
+            kl_models_weights: List[Union[Tuple[Policy, float], Tuple[Policy, float, float]]] = None
     ):
         self.rollout_generator = rollout_generator
 
@@ -101,6 +101,11 @@ class PPO:
         self.timer = time.time_ns() // 1_000_000
         self.jit_tracer = None
 
+        if kl_models_weights is not None:
+            for i in range(len(kl_models_weights)):
+                assert len(kl_models_weights[i]) in (2, 3)
+                if len(kl_models_weights[i]) == 2:
+                    kl_models_weights[i] = kl_models_weights[i] + (None,)
         self.kl_models_weights = kl_models_weights
 
     def update_reward_norm(self, rewards: np.ndarray) -> np.ndarray:
@@ -313,7 +318,8 @@ class PPO:
         tot_value_loss = 0
         total_kl_div = 0
         tot_clipped = 0
-        tot_kl_other_models = 0
+        tot_kl_other_models = np.zeros(len(self.kl_models_weights))
+        tot_kl_coeffs = np.zeros(len(self.kl_models_weights))
 
         n = 0
 
@@ -383,15 +389,19 @@ class PPO:
                 else:
                     entropy_loss = entropy
 
-                kl_other_models = 0
+                kl_loss = 0
                 if self.kl_models_weights is not None:
-                    for model, kl_coef in self.kl_models_weights:
+                    for k, (model, kl_coef, half_life) in enumerate(self.kl_models_weights):
+                        if half_life is not None:
+                            kl_coef *= np.exp(np.log(0.5) * self.total_steps / half_life)
                         with torch.no_grad():
-                            # obs = tuple(o.copy() for o in obs) if isinstance(obs, tuple) else obs.copy()
                             dist_other = model.get_action_distribution(obs)
-                        kl_other_models += kl_coef * kl_divergence(dist, dist_other).mean()
+                        div = kl_divergence(dist, dist_other).mean()
+                        tot_kl_other_models[k] += div
+                        tot_kl_coeffs[k] = kl_coef
+                        kl_loss += kl_coef * div
 
-                loss = ((policy_loss + self.ent_coef * entropy_loss + self.vf_coef * value_loss + kl_other_models)
+                loss = ((policy_loss + self.ent_coef * entropy_loss + self.vf_coef * value_loss + kl_loss)
                         / (self.batch_size / self.minibatch_size))
 
                 if not torch.isfinite(loss).all():
@@ -424,7 +434,6 @@ class PPO:
                 tot_entropy_loss += entropy_loss.item()
                 tot_value_loss += value_loss.item()
                 tot_clipped += th.mean((th.abs(ratio - 1) > self.clip_range).float()).item()
-                tot_kl_other_models += kl_other_models
                 n += 1
                 # pb.update(self.minibatch_size)
 
@@ -440,7 +449,8 @@ class PPO:
         assert n > 0
 
         postcompute = torch.cat([param.view(-1) for param in self.agent.actor.parameters()])
-        self.logger.log({
+
+        log_dict = {
             "ppo/loss": tot_loss / n,
             "ppo/policy_loss": tot_policy_loss / n,
             "ppo/entropy_loss": tot_entropy_loss / n,
@@ -449,8 +459,15 @@ class PPO:
             "ppo/clip_fraction": tot_clipped / n,
             "ppo/epoch_time": (t1 - t0) / (1e6 * self.epochs),
             "ppo/update_magnitude": th.dist(precompute, postcompute, p=2),
-            "ppo/kl_other_models": tot_kl_other_models / n,
-        }, step=iteration, commit=False)  # Is committed after when calculating fps
+        }
+
+        if len(self.kl_models_weights) > 0:
+            log_dict.update({f"ppo/kl_div_model_{i}": tot_kl_other_models[i] / n
+                             for i in range(len(self.kl_models_weights))})
+            log_dict.update({f"ppo/kl_coeff_model_{i}": tot_kl_coeffs[i]
+                             for i in range(len(self.kl_models_weights))})
+
+        self.logger.log(log_dict, step=iteration, commit=False)  # Is committed after when calculating fps
 
     def load(self, load_location, continue_iterations=True):
         """

rocket_learn/rollout_generator/redis/redis_rollout_worker.py
@@ -46,11 +46,11 @@ class RedisRolloutWorker:
     """
 
     def __init__(self, redis: Redis, name: str, match: Match,
-                 past_version_prob=.2, evaluation_prob=0.01, sigma_target=2,
+                 past_version_prob=.2, evaluation_prob=0.01, sigma_target=1,
                  dynamic_gm=True, streamer_mode=False, send_gamestates=True,
                  send_obs=True, scoreboard=None, pretrained_agents=None,
                  human_agent=None, force_paging=False, auto_minimize=True,
-                 local_cache_name=None, gamemode_weights=None, ):
+                 local_cache_name=None, gamemode_weights=None, full_team_evaluations=False):
         # TODO model or config+params so workers can recreate just from redis connection?
         self.redis = redis
         self.name = name
@@ -82,7 +82,10 @@ class RedisRolloutWorker:
         self.gamemode_weights = gamemode_weights
         if self.gamemode_weights is not None:
             assert np.isclose(sum(self.gamemode_weights.values()), 1), "gamemode_weights must sum to 1"
+        self.gamemode_exp_per_episode_ema = {}
         self.local_cache_name = local_cache_name
+
+        self.full_team_evaluations = full_team_evaluations
 
         self.uuid = str(uuid4())
         self.redis.rpush(WORKER_IDS, self.uuid)
@@ -115,7 +118,9 @@ class RedisRolloutWorker:
 
     def _get_opponent_ids(self, n_new, n_old, pretrained_choice):
         # Get qualities
-        gamemode = f"{(n_new + n_old) // 2}v{(n_new + n_old) // 2}"
+        assert (n_new + n_old) % 2 == 0
+        per_team = (n_new + n_old) // 2
+        gamemode = f"{per_team}v{per_team}"
         latest_id = self.redis.get(LATEST_RATING_ID).decode("utf-8")
         latest_key = f"{latest_id}-stochastic"
         if n_old == 0:
@@ -130,11 +135,17 @@ class RedisRolloutWorker:
             sigmas = np.array([r.sigma for r in values])
             probs = np.clip(sigmas - self.sigma_target, a_min=0, a_max=None)
             s = probs.sum()
-            if s == 0 or np.random.normal(0, 1) > self.sigma_target:
-                probs = np.ones_like(probs) / len(probs)
+            if s == 0:  # No versions with high sigma available
+                if np.random.normal(0, self.sigma_target) > 1:
+                    # Some chance of doing a match with random versions, so they might correct themselves
+                    probs = np.ones_like(probs) / len(probs)
+                else:
+                    return [-1] * n_old, [latest_rating] * n_old
             else:
                 probs /= s
             versions = [np.random.choice(len(keys), p=probs)]
+            if self.full_team_evaluations:
+                versions = versions * per_team
             target_rating = values[versions[0]]
             n_old -= 1
         elif pretrained_choice is not None:  # pretrained agent chosen, just need index generation
@@ -155,29 +166,48 @@ class RedisRolloutWorker:
         # This is to prevent unrealistic scenarios,
         # like for instance ratings of [100, 0] vs [100, 0], which is technically fair but not useful
         probs = np.zeros(len(keys))
-        for i, rating in enumerate(values):
-            if n_new == 0 and i == versions[0]:
-                continue  # Don't add more of the same agent in evaluation matches
-            p = probability_NvsM([rating], [target_rating])
-            probs[i] = (p * (1 - p)) ** (2 / (n_old + n_new))  # Be a little bit less strict the more players there are
-        probs /= probs.sum()
+        if n_new == 0 and self.full_team_evaluations:
+            for i, rating in enumerate(values):
+                if i == versions[0]:
+                    p = 0  # Don't add more of the same agent in evaluation matches
+                else:
+                    p = probability_NvsM([rating] * per_team, [target_rating] * per_team)
+                probs[i] = p
+            opponent = np.random.choice(len(probs), p=probs)
+            if np.random.random() < 0.5:  # Randomly do blue/orange
+                versions = versions + [opponent] * per_team
+            else:
+                versions = [opponent] * per_team + versions
+            return [keys[i] for i in versions], [values[i] for i in versions]
+        else:
+            for i, rating in enumerate(values):
+                if n_new == 0 and i == versions[0]:
+                    continue  # Don't add more of the same agent in evaluation matches
+                p = probability_NvsM([rating], [target_rating])
+                probs[i] = (p * (1 - p)) ** ((n_new + n_old) // 2)  # Be less lenient the more players there are
+            probs /= probs.sum()
 
-        old_versions = np.random.choice(len(probs), size=n_old, p=probs, replace=True).tolist()
-        versions += old_versions
+            old_versions = np.random.choice(len(probs), size=n_old, p=probs, replace=True).tolist()
+            versions += old_versions
 
-        # Then calculate the full matchup, with just permutations of the selected versions (weighted by fairness)
-        matchups = []
-        qualities = []
-        for perm in itertools.permutations(versions):
-            it_ratings = [latest_rating if v == -1 else values[v] for v in perm]
-            mid = len(it_ratings) // 2
-            p = probability_NvsM(it_ratings[:mid], it_ratings[mid:])
-            matchups.append(perm)
-            qualities.append(p * (1 - p))  # From AlphaStar
-        qualities = np.array(qualities)
-        k = np.random.choice(len(matchups), p=qualities / qualities.sum())
-        return [-1 if i == -1 else keys[i] for i in matchups[k]], \
-               [latest_rating if i == -1 else values[i] for i in matchups[k]]
+            # Then calculate the full matchup, with just permutations of the selected versions (weighted by fairness)
+            matchups = []
+            qualities = []
+            for perm in itertools.permutations(versions):
+                it_ratings = [latest_rating if v == -1 else values[v] for v in perm]
+                mid = len(it_ratings) // 2
+                p = probability_NvsM(it_ratings[:mid], it_ratings[mid:])
+                if n_new == 0 and set(perm[:mid]) == set(perm[mid:]):  # Don't want team against team
+                    p = 0
+                matchups.append(perm)
+                qualities.append(p * (1 - p))  # From AlphaStar
+            qualities = np.array(qualities)
+            s = qualities.sum()
+            if s == 0:
+                return [-1] * (n_new + n_old), [latest_rating] * (n_new + n_old)
+            k = np.random.choice(len(matchups), p=qualities / s)
+            return [-1 if i == -1 else keys[i] for i in matchups[k]], \
+                   [latest_rating if i == -1 else values[i] for i in matchups[k]]
 
     @functools.lru_cache(maxsize=8)
     def _get_past_model(self, version):
@@ -205,24 +235,29 @@ class RedisRolloutWorker:
 
         return model
 
-    def select_gamemode(self):
+    def select_gamemode(self, equal_likelihood):
         mode_exp = {m.decode("utf-8"): int(v) for m, v in self.redis.hgetall(EXPERIENCE_PER_MODE).items()}
         modes = list(mode_exp.keys())
-        dist = np.array(list(mode_exp.values())) + 1
-        dist = dist / dist.sum()
-        if self.gamemode_weights is None:
-            target_dist = np.ones(len(modes))
+        if equal_likelihood:
+            mode = np.random.choice(modes)
         else:
-            target_dist = np.array([self.gamemode_weights[k] for k in modes])
-        target_dist = target_dist / np.array([sum(int(n) for n in m.split("v")) for m in modes])
-        target_dist = target_dist / target_dist.sum()
-        inv_dist = 1 - dist
-        inv_dist = inv_dist / inv_dist.sum()
+            dist = np.array(list(mode_exp.values())) + 1
+            dist = dist / dist.sum()
+            if self.gamemode_weights is None:
+                target_dist = np.ones(len(modes))
+            else:
+                target_dist = np.array([self.gamemode_weights[k] for k in modes])
+            mode_steps_per_episode = np.array(list(self.gamemode_exp_per_episode_ema.get(m, None) or 1 for m in modes))
 
-        dist = target_dist * inv_dist
-        dist = dist / dist.sum()
+            target_dist = target_dist / mode_steps_per_episode
+            target_dist = target_dist / target_dist.sum()
+            inv_dist = 1 - dist
+            inv_dist = inv_dist / inv_dist.sum()
 
-        mode = np.random.choice(modes, p=dist)
+            dist = target_dist * inv_dist
+            dist = dist / dist.sum()
+
+            mode = np.random.choice(modes, p=dist)
 
         b, o = mode.split("v")
         return int(b), int(o)
@@ -256,8 +291,10 @@ class RedisRolloutWorker:
             n += 1
             pretrained_choice = None
 
+            evaluate = np.random.random() < self.evaluation_prob
+
             if self.dynamic_gm:
-                blue, orange = self.select_gamemode()
+                blue, orange = self.select_gamemode(equal_likelihood=evaluate or self.streamer_mode)
             elif self.match._spawn_opponents is False:
                 blue = self.match.agents
                 orange = 0
@@ -280,7 +317,10 @@ class RedisRolloutWorker:
                 # TODO customizable past agent selection, should team only be same agent?
                 agents, pretrained_choice, versions, ratings = self._generate_matchup(blue + orange,
                                                                                       latest_version,
-                                                                                      pretrained_choice)
+                                                                                      pretrained_choice,
+                                                                                      evaluate)
+
+            evaluate = not any(isinstance(v, int) and v < 0 for v in versions)  # Might be changed in matchup code
 
             version_info = []
             for v, r in zip(versions, ratings):
@@ -291,8 +331,7 @@ class RedisRolloutWorker:
                 else:
                     version_info.append(f"{v} ({r.mu:.2f}±{2 * r.sigma:.2f})")
 
-            if not any(isinstance(v, int) and v < 0 for v in versions) \
-                    and not self.streamer_mode and self.human_agent is None:
+            if evaluate and not self.streamer_mode and self.human_agent is None:
                 print("Running evaluation game with versions:", version_info)
                 result = rocket_learn.utils.generate_episode.generate_episode(self.env, agents, evaluate=True,
                                                                               scoreboard=self.scoreboard)
@@ -317,7 +356,17 @@ class RedisRolloutWorker:
                 state = rollouts[0].infos[-2]["state"]
                 goal_speed = np.linalg.norm(state.ball.linear_velocity) * 0.036  # kph
                 str_result = ('+' if result > 0 else "") + str(result)
-                self.total_steps_generated += len(rollouts[0].observations) * len(rollouts)
+                episode_exp = len(rollouts[0].observations) * len(rollouts)
+                self.total_steps_generated += episode_exp
+
+                if self.dynamic_gm and not evaluate:
+                    mode = f"{blue}v{orange}"
+                    if mode in self.gamemode_exp_per_episode_ema:
+                        current_mean = self.gamemode_exp_per_episode_ema[mode]
+                        self.gamemode_exp_per_episode_ema[mode] = 0.98 * current_mean + 0.02 * episode_exp
+                    else:
+                        self.gamemode_exp_per_episode_ema[mode] = episode_exp
+
                 post_stats = f"Rollout finished after {len(rollouts[0].observations)} steps ({self.total_steps_generated} total steps), result was {str_result}"
                 if result != 0:
                     post_stats += f", goal speed: {goal_speed:.2f} kph"
@@ -352,25 +401,22 @@ class RedisRolloutWorker:
                 # t.start()
                 # time.sleep(0.01)
 
-    def _generate_matchup(self, n_agents, latest_version, pretrained_choice):
-        n_old = 0
-        if n_agents > 1:
-            r = np.random.random()
-
-            if r < self.evaluation_prob:
-                n_old = n_agents
-            else:
-                rand_choice = (r - self.evaluation_prob) / (1 - self.evaluation_prob)
-                if rand_choice < self.past_version_prob:
-                    n_old = np.random.randint(low=1, high=n_agents)
-                elif rand_choice < (self.past_version_prob + self.pretrained_total_prob):
-                    wheel_prob = self.past_version_prob
-                    for agent in self.pretrained_agents:
-                        wheel_prob += self.pretrained_agents[agent]
-                        if rand_choice < wheel_prob:
-                            pretrained_choice = agent
-                            n_old = np.random.randint(low=1, high=n_agents)
-                            break
+    def _generate_matchup(self, n_agents, latest_version, pretrained_choice, evaluate):
+        if evaluate:
+            n_old = n_agents
+        else:
+            n_old = 0
+            rand_choice = np.random.random()
+            if rand_choice < self.past_version_prob:
+                n_old = np.random.randint(low=1, high=n_agents)
+            elif rand_choice < (self.past_version_prob + self.pretrained_total_prob):
+                wheel_prob = self.past_version_prob
+                for agent in self.pretrained_agents:
+                    wheel_prob += self.pretrained_agents[agent]
+                    if rand_choice < wheel_prob:
+                        pretrained_choice = agent
+                        n_old = np.random.randint(low=1, high=n_agents)
+                        break
         n_new = n_agents - n_old
         versions, ratings = self._get_opponent_ids(n_new, n_old, pretrained_choice)
         agents = []

rocket_learn/utils/stat_trackers/common_trackers.py
@@ -41,7 +41,8 @@ class Demos(StatTracker):
     def update(self, gamestates: np.ndarray, mask: np.ndarray):
         players = gamestates[:, StateConstants.PLAYERS]
 
-        demos = players[-1, StateConstants.MATCH_DEMOLISHES] - players[0, StateConstants.MATCH_DEMOLISHES]
+        demos = np.clip(players[-1, StateConstants.MATCH_DEMOLISHES] - players[0, StateConstants.MATCH_DEMOLISHES],
+                        0, None)
         self.total_demos += np.sum(demos)
         self.count += demos.size
 
@@ -218,8 +219,8 @@ class AirTouch(StatTracker):
     def update(self, gamestates: np.ndarray, mask: np.ndarray):
         players = gamestates[:, StateConstants.PLAYERS]
         is_touch = np.asarray([a * b for a, b in
-                              zip(players[:, StateConstants.BALL_TOUCHED],
-                                  np.invert(players[:, StateConstants.ON_GROUND].astype(bool)))])
+                               zip(players[:, StateConstants.BALL_TOUCHED],
+                                   np.invert(players[:, StateConstants.ON_GROUND].astype(bool)))])
 
         self.total_touches += np.sum(is_touch)
         self.count += is_touch.size
@@ -305,13 +306,16 @@ class GoalSpeed(StatTracker):
         self.total_speed = 0
 
     def update(self, gamestates: np.ndarray, mask: np.ndarray):
-        if gamestates.ndim > 1 and len(gamestates) > 1:
-            end = gamestates[-2]
-            goal_speed = end[StateConstants.BALL_LINEAR_VELOCITY]
-            goal_speed = np.linalg.norm(goal_speed)
+        orange_diff = np.diff(gamestates[:, StateConstants.ORANGE_SCORE], append=np.nan)
+        blue_diff = np.diff(gamestates[:, StateConstants.BLUE_SCORE], append=np.nan)
 
-            self.total_speed += goal_speed / 27.78  # convert to km/h
-            self.count += 1
+        goal_frames = (orange_diff > 0) | (blue_diff > 0)
+
+        goal_speed = gamestates[goal_frames, StateConstants.BALL_LINEAR_VELOCITY]
+        goal_speed = np.linalg.norm(goal_speed, axis=-1).sum()
+
+        self.total_speed += goal_speed / 27.78  # convert to km/h
+        self.count += goal_speed.size
 
     def get_stat(self):
         return self.total_speed / (self.count or 1)
@@ -355,3 +359,47 @@ class CarOnGround(StatTracker):
 
     def get_stat(self):
         return 100 * self.total_ground / (self.count or 1)
+
+
+class Saves(StatTracker):
+    def __init__(self):
+        super().__init__("average_saves")
+        self.count = 0
+        self.total_saves = 0
+
+    def reset(self):
+        self.count = 0
+        self.total_saves = 0
+
+    def update(self, gamestates: np.ndarray, mask: np.ndarray):
+        players = gamestates[:, StateConstants.PLAYERS]
+
+        saves = np.clip(players[-1, StateConstants.MATCH_SAVES] - players[0, StateConstants.MATCH_SAVES],
+                        0, None)
+        self.total_saves += np.sum(saves)
+        self.count += saves.size
+
+    def get_stat(self):
+        return self.total_saves / (self.count or 1)
+
+
+class Shots(StatTracker):
+    def __init__(self):
+        super().__init__("average_shots")
+        self.count = 0
+        self.total_shots = 0
+
+    def reset(self):
+        self.count = 0
+        self.total_shots = 0
+
+    def update(self, gamestates: np.ndarray, mask: np.ndarray):
+        players = gamestates[:, StateConstants.PLAYERS]
+
+        shots = np.clip(players[-1, StateConstants.MATCH_SHOTS] - players[0, StateConstants.MATCH_SHOTS],
+                        0, None)
+        self.total_shots += np.sum(shots)
+        self.count += shots.size
+
+    def get_stat(self):
+        return self.total_shots / (self.count or 1)